cf550a349ab893bfda8951101fc2dde70ba17afe
will <sopwill3@gmail.com>
2023-03-04 16:22:47 -0700

    Added the qualtrics connector. All that is left is injecting the other post information from a csv file

--- qualtrics-scripts/qualtrics-msmt-connector.js
+++ qualtrics-scripts/qualtrics-msmt-connector.js
@@ -12,25 +12,28 @@
 
 
 
-// PSUEDO-CODE
-  // Generate a link in Qualtics with
-    // All five images urls in order
-    // The Qulatrics ID
-  
-  // Use that link to create the iFrame
-
-
 // Get the URL params
-
 let augusto = "${e://Field/agnostic_augusto_url}".split("/")
 let jordy = "${e://Field/agnostic_jordy_url}".split("/")
 let kicis = "${e://Field/agnostic_kicis_url}".split("/")
 let melo = "${e://Field/agnostic_melo_url}".split("/")
 let martins = "${e://Field/agnostic_martins_url}".split("/")
-augusto = augusto[3].split(".")[0]
-jordy = jordy[3].split(".")[0]
-kicis = kicis[3].split(".")[0]
-melo = melo[3].split(".")[0]
-martins = martins[3].split(".")[0]
+augusto = augusto[3].split(".")[0] + "-cropped"
+jordy = jordy[3].split(".")[0] + "-cropped"
+kicis = kicis[3].split(".")[0] + "-cropped"
+melo = melo[3].split(".")[0] + "-cropped"
+martins = martins[3].split(".")[0] + "-cropped"
+let qualtricsId = "${e://Field/ResponseID}"
+const server_name = "http://localhost:8080/"
+const access_code = "466049"
+const params = "?url1=" + augusto + "&url2=" + jordy + "&url3=" + kicis + "&url4=" + melo + "&url5=" + martins + "&userid=" + qualtricsId
+const MSMT_url = server_name + access_code + params
+
+console.log(MSMT_url)
+
+
+const iframe = document.getElementById('msmt_iframe');
+iframe.src = MSMT_url; 
+
 
 // Example Response ID: R_3e5p3N9HduTE88B
